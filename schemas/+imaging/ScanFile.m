359cea4f2970c7fdda208720d1dfc40b21dc8e7a
Alvaro Luna <alvalunasan@gmail.com>
2020-06-15 17:50:08 -0400

    Correct aquisiton - imaging typo

--- schemas/+imaging/ScanFile.m
+++ schemas/+imaging/ScanFile.m
@@ -1,5 +1,5 @@
 %{
--> acquisition.Scan
+-> imaging.Scan
 acquisition_number  : int             # acquisition number of a given scan
 file_number         : int             # file number of a given acquisition
 ---
@@ -18,7 +18,7 @@ classdef ScanFile < dj.Computed
             patt_file_number = '_[0-9]{5}\.';
             patt_tiff_file   = '\.tif$';
             
-            imaging_directory = fetch1(acquisition.Scan & key, 'scan_directory');
+            imaging_directory = fetch1(imaging.Scan & key, 'scan_directory');
             dir_info          = dir(imaging_directory);
             dir_info          = {dir_info(:).name};
             
@@ -37,6 +37,7 @@ classdef ScanFile < dj.Computed
                        key.acquisition_number = str2double(acq_string{1}(2:end-1));
                        key.file_number        = str2double(number_string{1}(2:end-1));
                        key.scan_filename      = tiff_filename;
+                       key
                        self.insert(key)
                    else
                        fprintf('imaging name does not match expected format: subject_date_???_XXXXX_YYYYY.tiff %s\n',tiff_filename)
