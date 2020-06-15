1e7536b5f6ce22d876730753787c4823ae8e74f8
Alvaro Luna <alvalunasan@gmail.com>
2020-06-15 17:36:13 -0400

    Scan file table definition

--- schemas/+imaging/ScanFile.m
+++ schemas/+imaging/ScanFile.m
@@ -1,8 +1,9 @@
 %{
 -> acquisition.Scan
-file_number    : int             # file number of a given scan
+acquisition_number  : int             # acquisition number of a given scan
+file_number         : int             # file number of a given acquisition
 ---
-scan_filename  : varchar(255)
+scan_filename       : varchar(255)
 %}
 
 
@@ -12,9 +13,40 @@ classdef ScanFile < dj.Computed
         function makeTuples(self, key)
             
             % find subject and date from acquisition.Session table
-            key.file_number   = 0;
             
-            self.insert(key)
+            patt_acq_number  = '_[0-9]{5}_';
+            patt_file_number = '_[0-9]{5}\.';
+            patt_tiff_file   = '\.tif$';
+            
+            imaging_directory = fetch1(acquisition.Scan & key, 'scan_directory');
+            dir_info          = dir(imaging_directory);
+            dir_info          = {dir_info(:).name};
+            
+            tiff_idx = ~cellfun(@isempty,regexp(dir_info,patt_tiff_file),'UniformOutput',true);
+            dir_info = dir_info(tiff_idx);
+            
+            if ~isempty(dir_info)
+               for i=1:length(dir_info)
+                   
+                   tiff_filename = dir_info{i};
+                   acq_string = regexp(tiff_filename, patt_acq_number, 'match');
+                   number_string = regexp(tiff_filename, patt_file_number, 'match');
+                   
+                   if (length(acq_string) == 1 && length(number_string) == 1)
+                                              
+                       key.acquisition_number = str2double(acq_string{1}(2:end-1));
+                       key.file_number        = str2double(number_string{1}(2:end-1));
+                       key.scan_filename      = tiff_filename;
+                       self.insert(key)
+                   else
+                       fprintf('imaging name does not match expected format: subject_date_???_XXXXX_YYYYY.tiff %s\n',tiff_filename)
+                   end
+               end
+                
+            else
+                fprintf('no tiff files found in directory %s\n',imaging_directory)
+                return
+            end
             
         end
     end
