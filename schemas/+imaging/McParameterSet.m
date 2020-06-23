a45a7dee9aaddb16dcf228760aab9f9ac9b96f24
Alvaro Luna <alvalunasan@gmail.com>
2020-06-23 17:22:18 -0400

    McParameterSetParameter as Part McParameterSet as Manual

--- schemas/+imaging/McParameterSet.m
+++ schemas/+imaging/McParameterSet.m
@@ -4,11 +4,18 @@
 mc_parameter_set_id:   int    # parameter set id
 %}
 
-classdef McParameterSet < dj.Lookup
-    properties
-        contents = {
-            'LinearNormalized', 1
-            'NonLinearNormalized', 1
-            }
+classdef McParameterSet < dj.Manual
+    methods
+        function insert(self, key)
+            
+            insert@dj.Manual(self, key);
+            make(imaging.McParameterSetParameter, key)
+        end
     end
+%     properties
+%         contents = {
+%             'LinearNormalized', 1
+%             'NonLinearNormalized', 1
+%             }
+%     end
 end
--- schemas/+imaging/McParameterSetParameter.m
+++ schemas/+imaging/McParameterSetParameter.m
@@ -6,44 +6,52 @@
 value         : blob     # value of parameter
 %}
 
-classdef McParameterSetParameter < dj.Computed
+classdef McParameterSetParameter < dj.Part
     properties(SetAccess=protected)
         master   = imaging.McParameterSet
     end
-    methods (Access=protected)
-        function makeTuples(self, key)
+    methods
+        function make(self, key)
+            
+            paramKey.mcorr_method = key.mcorr_method;
+            query = imaging.McParameter & paramKey;
+            parameters = query.fetchn('mc_parameter_name');
             
             tableParam  =cell2table({
-                'LinearNormalized' , 1 , 'mc_max_shift' , 15;
-                'LinearNormalized' , 1 , 'mc_max_iter' , 5;
-                'LinearNormalized' , 1 , 'mc_extra_param' , false;
-                'LinearNormalized' , 1 , 'mc_stop_below_shift' , 0.3;
-                'LinearNormalized' , 1 , 'mc_black_tolerance' , -1;
-                'LinearNormalized' , 1 , 'mc_median_rebin' , 10;
-                'NonLinearNormalized' , 1 , 'mc_max_shift' , [15 15];
-                'NonLinearNormalized' , 1 , 'mc_max_iter' , [5 2];
-                'NonLinearNormalized' , 1 , 'mc_stop_below_shift' , 0.3;
-                'NonLinearNormalized' , 1 , 'mc_black_tolerance' , -1;
-                'NonLinearNormalized' , 1 , 'mc_median_rebin' , 10;
-                }, 'VariableNames',{'mcorr_method' 'mc_parameter_set_id' 'mc_parameter_name' 'value'});
+                'LinearNormalized' , 'mc_max_shift' , 15;
+                'LinearNormalized' , 'mc_max_iter' , 5;
+                'LinearNormalized' , 'mc_extra_param' , false;
+                'LinearNormalized' , 'mc_stop_below_shift' , 0.3;
+                'LinearNormalized' , 'mc_black_tolerance' , -1;
+                'LinearNormalized' , 'mc_median_rebin' , 10;
+                
+                'NonLinearNormalized' , 'mc_max_shift' , [15 15];
+                'NonLinearNormalized' , 'mc_max_iter' , [5 2];
+                'NonLinearNormalized' , 'mc_stop_below_shift' , 0.3;
+                'NonLinearNormalized' , 'mc_black_tolerance' , -1;
+                'NonLinearNormalized' , 'mc_median_rebin' , 10;
+                
+                }, 'VariableNames',{'mcorr_method' 'mc_parameter_name' 'value'});
             
             tableParam.mcorr_method = categorical(tableParam.mcorr_method);
             tableParam.mc_parameter_name = categorical(tableParam.mc_parameter_name);
             
-            value_cell = tableParam{key.mcorr_method == tableParam.mcorr_method & ...
-                key.mc_parameter_set_id == tableParam.mc_parameter_set_id & ...
-                key.mc_parameter_name == tableParam.mc_parameter_name, 'value'};
+            for i=1:length(parameters)
+                key.mc_parameter_name = parameters{i};
+                value_cell = tableParam{tableParam.mcorr_method == key.mcorr_method & ...
+                                        tableParam.mc_parameter_name == key.mc_parameter_name, 'value'};
             
-            if size(value_cell,1) > 1
-                warning('More than one value for this key found: %s %d %s',key.mcorr_method, key.mc_parameter_set_id, key.mc_parameter_name)
-                key.value = value_cell{1,1};
-            elseif size(value_cell,1) == 0
-                error('No value for this key found: : %s %d %s',key.mcorr_method, key.mc_parameter_set_id, key.mc_parameter_name)
-            else
-                key.value = value_cell{1,1};
+                if size(value_cell,1) > 1
+                    warning('More than one value for this key found: %s %d %s',key.mcorr_method, key.mc_parameter_set_id, key.mc_parameter_name)
+                    key.value = value_cell{1,1};
+                elseif size(value_cell,1) == 0
+                    error('No value for this key found: : %s %d %s',key.mcorr_method, key.mc_parameter_set_id, key.mc_parameter_name)
+                else
+                    key.value = value_cell{1,1};
+                end
+            
+                self.insert(key);
             end
-            
-            self.insert(key);
             
         end
     end
