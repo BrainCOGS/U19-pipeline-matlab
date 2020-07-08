a1a147185fb868f8e242ead271c04dd0a2aeb959
vr6 <aa>
2020-07-08 19:44:36 -0400

    Video table dj.manual

--- schemas/+behavior/TowersBlockTrialVideo.m
+++ schemas/+behavior/TowersBlockTrialVideo.m
@@ -1,16 +1,13 @@
 %{
--> behavior.TowersBlockTrialVideo
+-> behavior.TowersBlockTrial
 ---
 video:                      longblob # frame by frame video for trial
 %}
 
-classdef TowersBlockTrialVideo < dj.Part
-    properties(SetAccess=protected)
-        master = behavior.TowersBlockTrial
-    end
-    methods
-        function make(self, key)
-            self.insert(key);
-        end
-    end
+classdef TowersBlockTrialVideo < dj.Manual
+%     methods
+%         %function insert(self, key)
+%         %    self.insert(key);
+%         %end
+%     end
 end
