8b8133d893fe4a1dd6b0ceca2ef2aaafa342dc3c
Alvaro Luna <alvalunasan@gmail.com>
2020-07-23 10:11:49 -0400

    TestExStorage

---
+++ schemas/+behavior/TestTowersBlockTrialVideoExStorage.m
@@ -0,0 +1,13 @@
+%{
+-> behavior.TowersBlockTrial
+---
+external_video:     blob@trialvideos 
+%}
+
+classdef TestTowersBlockTrialVideoExStorage < dj.Manual
+%     methods
+%         %function insert(self, key)
+%         %    self.insert(key);
+%         %end
+%     end
+end
