ed1fd36510f4eb8a7a133998343b9c7fd7957162
Alvaro Luna <alvalunasan@gmail.com>
2020-08-05 11:26:09 -0400

    tif header is Software field for meso, image description for 2photon

--- utils/+u19_dj_utils/parse_tif_header.m
+++ utils/+u19_dj_utils/parse_tif_header.m
@@ -14,7 +14,11 @@ if nargin < 2; skipBehavSync = false; end
 header                      = imfinfo(tifFn);
 
 %% general image info
-scopeStr                    = header(1).ImageDescription;
+if isfield(header(1), 'Software')
+    scopeStr                    = header(1).Software;
+else
+    scopeStr                    = header(1).ImageDescription;
+end
 parsedInfo.Filename         = header(1).Filename;
 parsedInfo.Width            = header(1).Width;
 parsedInfo.Height           = header(1).Height;
