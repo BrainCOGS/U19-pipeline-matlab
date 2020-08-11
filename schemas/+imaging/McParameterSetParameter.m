3bc1c5959ee7e31cce685efd952d9596a6e5be4f
Alvalunasan <alvalunasan@gmail.com>
2020-08-11 08:52:17 -0400

    MotionCorrection v1

---
+++ schemas/+imaging/McParameterSetParameter.m
@@ -0,0 +1,57 @@
+%{
+# pre-saved parameter values
+-> imaging.McParameterSet
+-> imaging.McParameter
+---
+value         : blob     # value of parameter
+%}
+
+classdef McParameterSetParameter < dj.Part
+    properties(SetAccess=protected)
+        master   = previousimaging.McParameterSet
+    end
+    methods
+        function make(self, key)
+            
+            query = imaging.McParameter & key;
+            parameters = query.fetchn('mc_parameter_name');
+            
+            tableParam  =cell2table({
+                'LinearNormalized' , 'mc_max_shift' , 15;
+                'LinearNormalized' , 'mc_max_iter' , 5;
+                'LinearNormalized' , 'mc_extra_param' , false;
+                'LinearNormalized' , 'mc_stop_below_shift' , 0.3;
+                'LinearNormalized' , 'mc_black_tolerance' , -1;
+                'LinearNormalized' , 'mc_median_rebin' , 10;
+                
+                'NonLinearNormalized' , 'mc_max_shift' , [15 15];
+                'NonLinearNormalized' , 'mc_max_iter' , [5 2];
+                'NonLinearNormalized' , 'mc_stop_below_shift' , 0.3;
+                'NonLinearNormalized' , 'mc_black_tolerance' , -1;
+                'NonLinearNormalized' , 'mc_median_rebin' , 10;
+                
+                }, 'VariableNames',{'mcorr_method' 'mc_parameter_name' 'value'});
+            
+            tableParam.mc_method = categorical(tableParam.mc_method);
+            tableParam.mc_parameter_name = categorical(tableParam.mc_parameter_name);
+            
+            for i=1:length(parameters)
+                key.mc_parameter_name = parameters{i};
+                value_cell = tableParam{tableParam.mc_method == key.mc_method & ...
+                                        tableParam.mc_parameter_name == key.mc_parameter_name, 'value'};
+            
+                if size(value_cell,1) > 1
+                    warning('More than one value for this key found: %s %d %s',key.mc_method, key.mc_parameter_set_id, key.mc_parameter_name)
+                    key.value = value_cell{1,1};
+                elseif size(value_cell,1) == 0
+                    error('No value for this key found: : %s %d %s',key.mc_method, key.mc_parameter_set_id, key.mc_parameter_name)
+                else
+                    key.value = value_cell{1,1};
+                end
+            
+                self.insert(key);
+            end
+            
+        end
+    end
+end
---
+++ schemas/+imaging/MotionCorrection.m
@@ -0,0 +1,116 @@
+%{
+-> imaging.FieldOfView
+-> imaging.McParameterSet       # meta file, frameMCorr-method
+---
+%}
+
+classdef MotionCorrection < dj.Imported
+    methods (Access=protected)
+        function makeTuples(self, key)
+            
+            
+            %Get Parameters from McParameterSetParameter table
+            params        = u19_dj_utils.getParametersFromQuery(imaging.McParameterSetParameter & key);
+            
+            %Correct mc_black_tolerance parameter
+            if params.mc_black_tolerance < 0
+                params.mc_black_tolerance = nan;
+            end
+            
+            %Define cfg.mcorr with parameters (as in meso pipeline)
+            if contains(key.mcorr_method,'NonLinear')
+                cfg.mcorr    = {params.mc_max_shift, params.mc_max_iter, params.mc_stop_below_shift, ...
+                    params.mc_black_tolerance, params.mc_median_rebin};
+            else
+                cfg.mcorr    = {params.mc_max_shift, params.mc_max_iter, params.mc_extra_param, ...
+                    params.mc_stop_below_shift, params.mc_black_tolerance, params.mc_median_rebin};
+            end
+                        
+            %Get scan directory
+            fov_directory  = formatFilePath(fetch1(imaging.FieldOfView & key,'fov_directory'),true,true);
+            
+            
+            %% call functions to compute motioncorrectionWithinFile and AcrossFiles and insert into the tables
+            fprintf('==[ PROCESSING ]==   %s\n', fov_directory);
+            
+            % Determine whether or not we need to use frame skipping to select only the first channel
+            [order,movieFiles]            = fetchn(imaging.FieldOfViewFile & key, 'file_number', 'fov_filename');
+            movieFiles                    = cellfun(@(x)(fullfile(fov_directory,x)),movieFiles(order),'uniformoutput',false); % full path
+
+            info                          = cv.imfinfox(movieFiles{1}, true);
+            if numel(info.channels) > 1
+                cfg.mcorr{end+1}            = [0, numel(info.channels)-1];
+            end
+            
+            % run motion correction
+            if isempty(gcp('nocreate')); poolobj = parpool; end
+            
+            [frameMCorr, fileMCorr]       = getMotionCorrection(movieFiles, false, 'off', cfg.mcorr{:});
+            
+            %% insert within file correction meso.motioncorrectionWithinFile
+            within_key                        = key;
+            within_key.file_number            = [];
+            within_key.within_file_x_shifts   = [];
+            within_key.within_file_y_shifts   = [];
+            within_key.within_reference_image = [];
+            within_key                        = repmat(within_key,[1 numel(frameMCorr)]);
+            
+            for iFile = 1:numel(frameMCorr)
+                within_key(iFile).file_number                   = iFile;
+                within_key(iFile).within_file_x_shifts          = frameMCorr(iFile).xShifts;
+                within_key(iFile).within_file_y_shifts          = frameMCorr(iFile).yShifts;
+                within_key(iFile).within_reference_image        = frameMCorr(iFile).reference;
+            end
+            
+            
+            %% insert within file correction meso.motioncorrectionAcrossFile
+            across_key                             = key;
+            across_key.cross_files_x_shifts        = fileMCorr.xShifts;
+            across_key.cross_files_y_shifts        = fileMCorr.yShifts;
+            across_key.cross_files_reference_image = fileMCorr.reference;
+            
+            class(fileMCorr.reference)
+            size(fileMCorr.reference)
+            
+            %% compute and save some stats as .mat files, intermediate step used downstream in the segmentation code
+            movieName                     = stripPath(movieFiles);
+            parfor iFile = 1:numel(movieFiles)
+                computeStatistics(movieName{iFile}, movieFiles{iFile}, frameMCorr(iFile), false);
+            end
+            
+            %% insert key
+            self.insert(key);
+            insert(imaging.MotionCorrectionWithinFile, within_key)
+            insert(imaging.MotionCorrectionAcrossFiles, across_key)
+            
+        end
+    end
+end
+
+%%
+%---------------------------------------------------------------------------------------------------
+function [statsFile, activity] = computeStatistics(movieName, movieFile, frameMCorr, recomputeStats)
+
+fprintf(' :   %s\n', movieName);
+
+% Fluorescence activity raw statistics
+statsFile                   = regexprep(movieFile, '[.][^.]+$', '.stats.mat');
+if recomputeStats ||  ~exist(statsFile, 'file')
+    % Load raw data with per-file motion correction
+    F                         = cv.imreadsub(movieFile, {frameMCorr,false});
+    [stats,metric,tailProb]   = highTailActivityMetric(F);
+    clear F;
+    
+    info                      = cv.imfinfox(movieFile);
+    info.movieFile            = stripPath(movieFile);
+    outputFile                = statsFile;
+    if ~isfile(outputFile)
+        parsave(outputFile, info, stats, metric, tailProb);
+    end
+else
+    metric                    = load(statsFile, 'metric');
+    tailProb                  = metric.metric.tailProb;
+end
+activity                    = tailProb;
+
+end
