4e9986978aa042b91e47028420a4d156eeb505fd
Alvaro Luna <alvalunasan@gmail.com>
2020-08-14 10:31:51 -0400

    Check what goes out from imreadx

--- schemas/+previousimaging/MotionCorrection.m
+++ schemas/+previousimaging/MotionCorrection.m
@@ -40,6 +40,8 @@ classdef MotionCorrection < dj.Imported
             info                          = cv.imfinfox(movieFiles{1}, true);
             info
             file                          = cv.imreadx(movieFiles{1});
+            size(file)
+            class(file)
             if numel(info.channels) > 1
                 cfg.mcorr{end+1}            = [0, numel(info.channels)-1];
             end
