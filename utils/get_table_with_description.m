12465147bf04c2806f807d100f4993c2457a2721
Alvaro Luna <alvalunasan@gmail.com>
2022-04-18 11:56:11 -0500

    Added prefered order for tables with description

--- utils/get_table_with_description.m
+++ utils/get_table_with_description.m
@@ -7,15 +7,15 @@ function [keys,description_field] = get_table_with_description(table)
 % Outputs  
 % keys                = primary keys of table as cell array
 % description_field   = description field(s) as a cell array
-
+ 
 %Get idx of description fields
 table_fields = {table.tableHeader.attributes.name};
 idx_param_description = contains(table_fields,'description');
 description_field = table_fields(idx_param_description);
-
+ 
 %Get primaryKey field
 key_field = table.primaryKey;
-
+ 
 % Assert assumptions
 if isempty(description_field)
     error('There is no description field in this table: %s', table.className);
@@ -24,10 +24,20 @@ elseif length(description_field) > 1
 elseif length(key_field) > 1
     error('This table has a composite primary key:%s%s', newline, strjoin(key_field, newline))
 end
-
+ 
 %Fetch data
 [keys,description_field] = fetchn(table, key_field{:},description_field{:});
-
+ 
+%If none present in keys, put it first (always none is default)
+id_none = find(strcmp(keys, 'none'));
+if id_none
+    id_others = 1:length(keys);
+    id_others(id_none) = [];
+    keys = keys([id_none id_others]);
+    description_field = description_field([id_none id_others]);
+end
+ 
+ 
 % Join keys & description for description_field
 if ~iscell(keys)
     str_keys = cellfun(@num2str, num2cell(keys), 'un', 0);
@@ -35,6 +45,6 @@ if ~iscell(keys)
 else
     description_field=strcat(keys,{': '},description_field);
 end
-
+ 
     
 end
