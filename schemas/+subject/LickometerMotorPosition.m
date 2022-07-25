60e8159ccd2f3af39a3de70d49a599b224275fda
Alvaro Luna <alvalunasan@gmail.com>
2022-07-25 15:47:27 -0400

    Added LickometerMotorPosition table

---
+++ schemas/+subject/LickometerMotorPosition.m
@@ -0,0 +1,22 @@
+%{
+-> subject.Subject
+---
+ml_position:      float        # mediolateral default position for motor
+ap_position:      float        # anterioposterior default position for motor
+dv_position:      float        # dorsoventral default position for motor
+%}
+
+% AX1 ML Mediolateral
+% AX2 AP Anteroposterior
+% AX3 DV Dorsoventral
+
+classdef LickometerMotorPosition < dj.Manual
+    
+    properties (Constant = true)
+       
+        axis_struct = struct('ml_position',1, 'ap_position', 2, 'dv_position', 3);
+        
+    end
+    
+    
+end
