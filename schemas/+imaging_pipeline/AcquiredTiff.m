e955425721e698e0f4403eefd350ce6b7f49d898
Alvaro Luna <alvalunasan@gmail.com>
2022-08-10 15:19:07 -0400

    not nan degrees

--- schemas/+imaging_pipeline/AcquiredTiff.m
+++ schemas/+imaging_pipeline/AcquiredTiff.m
@@ -577,22 +577,25 @@ classdef AcquiredTiff < dj.Imported
                     end
                     
                     fov_key.fov_center_xy           = recInfo.ROI(iROI).centerXY;
-                    if isempty(fov_key.fov_center_xy) || isnan(fov_key.fov_center_xy)
+                    if isempty(fov_key.fov_center_xy)
                         fov_key.fov_center_xy  = -1;
                     end
                     
                     fov_key.fov_size_xy             = recInfo.ROI(iROI).sizeXY;
-                    if isempty(fov_key.fov_size_xy) || isnan(fov_key.fov_size_xy)
+                    if isempty(fov_key.fov_size_xy)
                         fov_key.fov_size_xy = -1;
                     end
                     
                     fov_key.fov_rotation_degrees    = recInfo.ROI(iROI).rotationDegrees;
-                    if isempty(fov_key.fov_rotation_degrees) || isnan(fov_key.fov_rotation_degrees) 
+                    if isempty(fov_key.fov_rotation_degrees)
+                        fov_key.fov_rotation_degrees = -1;
+                    end
+                    if isnan(fov_key.fov_rotation_degrees) 
                         fov_key.fov_rotation_degrees = -1;
                     end
                     
                     fov_key.fov_pixel_resolution_xy = recInfo.ROI(iROI).pixelResolutionXY;
-                    if isempty(fov_key.fov_pixel_resolution_xy) || isnan(fov_key.fov_pixel_resolution_xy)
+                    if isempty(fov_key.fov_pixel_resolution_xy)
                         fov_key.fov_pixel_resolution_xy = -1;
                     end
                     
