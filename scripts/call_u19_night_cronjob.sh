3cb0276dac40853ac09149d9adb39220740f6e79
Alvaro Luna <alvalunasan@gmail.com>
2022-09-28 11:37:37 -0400

    call cronjob script

---
+++ scripts/call_u19_night_cronjob.sh
@@ -0,0 +1,8 @@
+echo $(pwd)
+
+source /home/u19prod@pu.win.princeton.edu/.bashrc
+source /home/u19prod@pu.win.princeton.edu/.bash_profile
+
+cd "/home/u19prod@pu.win.princeton.edu/Datajoint_projs/U19-pipeline-matlab/"
+git pull
+matlab -nodisplay -nosplash -nodesktop -batch "run 'scripts/ingest_all.m';"
