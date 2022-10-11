b5d7d2bf8ef79ccb201694ea6faff507631d2657
Alvaro Luna <alvalunasan@gmail.com>
2022-10-11 14:37:19 -0400

    Include SyncImagingBehavior for autmation pipeline

--- scripts/populate_Imaging_AcquiredTiff.m
+++ scripts/populate_Imaging_AcquiredTiff.m
@@ -5,8 +5,10 @@ startup_u19_pipeline_matlab_spock
 
 if nargin < 1
     populate(imaging_pipeline.AcquiredTiff);
+    populate(imaging_pipeline.SyncImagingBehavior);
 else
     populate(imaging_pipeline.AcquiredTiff, key);
+    populate(imaging_pipeline.SyncImagingBehavior, key);
      
 end
 
