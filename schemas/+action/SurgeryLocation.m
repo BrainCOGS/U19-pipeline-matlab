ff76ed6f04be6cba2946a991a3fc0717bd7f5f42
Alvaro Luna <alvalunasan@gmail.com>
2022-10-11 11:16:29 -0400

    Update definiiton for Surgery and SurgeryLocation

--- schemas/+action/SurgeryLocation.m
+++ schemas/+action/SurgeryLocation.m
@@ -1,15 +1,15 @@
 %{
+# 
 -> action.Surgery
-device_idx:                   int
+device_idx                  : int                           # 
 ---
 -> lab.InsertionDevice
-hemisphere:                   enum('L', 'R', 'Bilateral')
--> reference.BrainLocation=null
-real_ap_coordinates:          decimal(5,2)             # anteroposterior coordinates in mm   
-real_dv_coordinates:          decimal(5,2)             # dorsoventral coordinates in mm
-real_ml_coordinates:          decimal(5,2)             # mediolateral coordinates in mm
-theta:                        decimal(5, 2)            # (deg) - elevation - rotation about the ml-axis [0, 180] - w.r.t the z+ axis
-phi:                          decimal(5, 2)            # (deg) - azimuth - rotation about the dv-axis [0, 360] - w.r.t the x+ axis
+hemisphere                  : enum('L','R','Bilateral')     # 
+real_ap_coordinates         : decimal(5,2)                  # anteroposterior coordinates in mm
+real_dv_coordinates         : decimal(5,2)                  # dorsoventral coordinates in mm
+real_ml_coordinates         : decimal(5,2)                  # mediolateral coordinates in mm
+phi_angle=null              : decimal(5,2)                  # phi angle for insertion
+theta_angle=null            : decimal(5,2)                  # theta angle for insertion
 %}
 
 
