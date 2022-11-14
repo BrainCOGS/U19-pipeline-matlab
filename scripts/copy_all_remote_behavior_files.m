391e01a251c093327d2c858674f30ee79fc4c299
Alvaro Luna <alvalunasan@gmail.com>
2022-11-14 15:33:04 -0500

    Function to move a single behavior file to cup (along with fig files)

--- scripts/copy_all_remote_behavior_files.m
+++ scripts/copy_all_remote_behavior_files.m
@@ -12,44 +12,15 @@ for i=1:length(session_info)
     if isfile(this_remote_filepath)
         % Get full new remote filepath
         [~, new_remote_file] = lab.utils.get_path_from_official_dir(session_info(i).new_remote_path_behavior_file);
-        remote_dir = fileparts(new_remote_file);
-        % If remote file is not there copy it
-        if ~isfile(new_remote_file)
-            % If remote folder is not there create it
-            if ~isfolder(remote_dir)
-                mkdir(remote_dir)
-            end
-            [status, msg] = copyfile(this_remote_filepath, new_remote_file);
-            if ~status
-                something_failed = 1;
-            end
-            %Copy fig files as well
-            local_dir = fileparts(this_remote_filepath);
-            % Locate same date fig files
-            filesList = dir(local_dir);
-            fileNames = {filesList.name};
-            session_str = strrep(session_info(i).session_date,'-','');
-            fig_files = regexpi(fileNames, ['\w*' session_str '_[0-9].fig'], 'match');
-            idx_fig_files = cellfun(@(x) ~isempty(x), fig_files);
-            
-            % Also look for fig files that are from multiple sessions on the same day
-            session_str2 = [session_str '_' num2str(session_info(i).session_number)];
-            fig_files2 = regexpi(fileNames, ['\w*' session_str2 '_[0-9].fig'], 'match');
-            idx_fig_files2 = cellfun(@(x) ~isempty(x), fig_files2);
-            idx_fig_files = idx_fig_files | idx_fig_files2;
-
-            fig_files = fileNames(idx_fig_files);
-            %For each same date fig file, copy it to new location
-            for j=1:length(fig_files)
-                new_fig_file = fullfile(remote_dir, fig_files{j});
-                local_fig_file = fullfile(local_dir, fig_files{j});
-                [status, msg] = copyfile(local_fig_file, new_fig_file);
-                if ~status
-                    something_failed = 1;
-                end
-            end
-                
+        
+        [status, this_msg] = copy_single_behavior_file_cup(this_remote_filepath, new_remote_file, ...
+            session_info(i).session_date, session_info(i).session_number);
+        % Report when something happened while copying files
+        if ~status
+            something_failed = 1;
+            msg = this_msg;
         end
+           
     end
 
     if something_failed
--- scripts/copy_remote_behavior_files.m
+++ scripts/copy_remote_behavior_files.m
@@ -19,49 +19,17 @@ for i=1:length(session_info)
     if isfile(session_info(i).local_path_behavior_file)
         % Get full new remote filepath
         [~, new_remote_file] = lab.utils.get_path_from_official_dir(session_info(i).new_remote_path_behavior_file);
-        remote_dir = fileparts(new_remote_file);
-        % If remote file is not there copy it
-        if ~isfile(new_remote_file)
-            % If remote folder is not there create it
-            if ~isfolder(remote_dir)
-                mkdir(remote_dir)
-            end
-            [status, msg] = copyfile(session_info(i).local_path_behavior_file, new_remote_file);
-            if ~status
-                final_status = 0;
-            end
-            %Copy fig files as well
-            local_dir = fileparts(session_info(i).local_path_behavior_file);
-            % Locate same date fig files
-            filesList = dir(local_dir);
-            fileNames = {filesList.name};
-            session_str = strrep(session_info(i).session_date,'-','');
-            fig_files = regexpi(fileNames, ['\w*' session_str '_[0-9].fig'], 'match');
-            idx_fig_files = cellfun(@(x) ~isempty(x), fig_files);
-            
-            % Also look for fig files that are from multiple sessions on the same day
-            session_str2 = [session_str '_' num2str(session_info(i).session_number)];
-            fig_files2 = regexpi(fileNames, ['\w*' session_str2 '_[0-9].fig'], 'match');
-            idx_fig_files2 = cellfun(@(x) ~isempty(x), fig_files2);
-            idx_fig_files = idx_fig_files | idx_fig_files2;
-
-            fig_files = fileNames(idx_fig_files);
-            %For each same date fig file, copy it to new location
-            for j=1:length(fig_files)
-                new_fig_file = fullfile(remote_dir, fig_files{j});
-                local_fig_file = fullfile(local_dir, fig_files{j});
-                [status, msg] = copyfile(local_fig_file, new_fig_file);
-                if ~status
-                    final_status = 0;
-                end
-            end
-            
-            
-                
+        [status, this_msg] = copy_single_behavior_file_cup(session_info(i).local_path_behavior_file, new_remote_file, ...
+            session_info(i).session_date, session_info(i).session_number);
+        % Report when something happened while copying files
+        if ~status
+            final_status = 0;
+            msg = this_msg;
         end
+        
     else
         % if local file does not exist then it is invalid session
         update(acquisition.SessionStarted & session_keys(i), 'invalid_session', 1)
     end
-
+    
 end
