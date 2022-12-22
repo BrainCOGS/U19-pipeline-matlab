a0c946ef381baf62a1a95acb227047b7fe402b64
Alvaro Luna <alvalunasan@gmail.com>
2022-12-22 09:42:03 -0600

    add recording system also acquisition type

--- schemas/+lab/+utils/add_recording_system.m
+++ schemas/+lab/+utils/add_recording_system.m
@@ -1,5 +1,9 @@
-function add_recording_system(system_name)
+function add_recording_system(system_name, acquisition_type)
 %Add new recording_system to location table, if already exist update system_type
+% Inputs
+% system_name = 
+
+
 
 new_key = struct;
 query_key = struct;
@@ -10,9 +14,11 @@ location_record = fetch(lab.Location & query_key);
 if isempty(location_record)
     new_key.location         = system_name;
     new_key.system_type      = "recording";
+    new_key.acquisition_type = acquisition_type;
     insert(lab.Location, new_key)
 else
     update(lab.Location & query_key, 'system_type', 'recording');
+    update(lab.Location & query_key, 'acquisition_type', acquisition_type);
 end
     
 
