92edfc3147b28b1d5a8b5069dded45e9500edec4
Alvaro Luna <alvalunasan@gmail.com>
2023-07-06 21:52:27 -0600

    copy video files

---
+++ scripts/copy_video_files/copy_remote_video_files.m
@@ -0,0 +1,40 @@
+function [final_status, msg] = copy_remote_video_files(location, video_type)
+
+if nargin == 1
+    video_type = "pupillometry";
+end
+
+conf = dj.config;
+
+%Get sessions that are not yet in pupillometry DB (most probable not being copied yet)
+key = struct();
+key.session_location = location;
+key.video_type = video_type;
+
+if video_type == "pupillometry"
+    raw_dir = conf.custom.PupillometryRootDataDir;
+    session_info = fetch(((acquisition.SessionStarted * acquisition.SessionVideo) - pupillometry.PupillometrySession) & key,'*');
+    
+else
+   error('Video copy not implemented for this video_type yet'); 
+end
+    %session_keys = fetch(((acquisition.SessionStarted * acquisition.SessionVideo) - pupillometry.PupillometrySession) & key);
+
+final_status = 1;
+msg = '';
+% For each of the sessions copy corresponding files
+for i=1:length(session_info)
+    
+    % Check if local file exists
+    if isfile(session_info(i).local_path_video_file)
+        new_video_file = fullfile(raw_dir, session_info(i).remote_path_video_file);
+        [status, this_msg] = copy_single_video_file_cup(session_info(i).local_path_behavior_file, ...
+            new_video_file);
+        % Report when something happened while copying files
+        if ~status
+            final_status = 0;
+            msg = this_msg;
+        end
+    end
+    
+end
---
+++ scripts/copy_video_files/copy_single_video_file_cup.m
@@ -0,0 +1,21 @@
+function [status, msg] = copy_single_video_file_cup(local_path_behavior_file, new_remote_file)
+% Copy a single video file to cup
+
+status = 1;
+msg = '';
+
+% Check if local file exists
+if isfile(local_path_behavior_file)
+    
+    remote_dir = fileparts(new_remote_file);
+    % If remote file is not there copy it
+    if ~isfile(new_remote_file)
+        % If remote folder is not there create it
+        if ~isfolder(remote_dir)
+            mkdir(remote_dir)
+        end
+        [status, msg] = copyfile(local_path_behavior_file, new_remote_file);
+           
+    end
+end
+
