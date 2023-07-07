8d6b9c55cc7d6bb57a0d96a0ce31ffa3003d3844
Alvaro Luna <alvalunasan@gmail.com>
2023-07-06 22:47:51 -0600

    video backup .bat

--- scripts/copy_video_files/copy_video_files_script.m
+++ scripts/copy_video_files/copy_video_files_script.m
@@ -2,5 +2,5 @@
 % Script to run in cronjob to do behavior backup copying files 
 [final_status, msg] = copy_remote_video_files(RigParameters.rig);
 if ~final_status
-    error_behavior_backup_notification_slack(RigParameters.rig, msg)
+    error_video_backup_notification_slack(RigParameters.rig, msg)
 end
