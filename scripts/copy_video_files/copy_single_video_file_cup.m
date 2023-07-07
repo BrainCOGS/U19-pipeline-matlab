9125d060bee3452e5148bc651150112cd2c43621
Alvaro Luna <alvalunasan@gmail.com>
2023-07-06 22:19:59 -0600

    fix typo video backup

--- scripts/copy_video_files/copy_single_video_file_cup.m
+++ scripts/copy_video_files/copy_single_video_file_cup.m
@@ -7,7 +7,7 @@ msg = '';
 % Check if local file exists
 if isfile(local_path_behavior_file)
     
-    local_directory = fileparts(local_path_behavior_file);
+    %local_directory = fileparts(local_path_behavior_file);
     remote_dir = fileparts(new_remote_file);
     % If remote file is not there copy it
     if ~isfile(new_remote_file)
@@ -15,10 +15,10 @@ if isfile(local_path_behavior_file)
         if ~isfolder(remote_dir)
             mkdir(remote_dir)
         end
-        copy_command = ['ROBOCOPY ' local_directory ' ' remote_dir ' /E'];
-        [status,msg] = system(copy_command);
+        %copy_command = ['ROBOCOPY ' local_directory ' ' remote_dir ' /E'];
+        %[status,msg] = system(copy_command);
 
-        %[status, msg] = copyfile(local_path_behavior_file, new_remote_file);
+        [status, msg] = copyfile(local_path_behavior_file, new_remote_file);
            
     end
 end
