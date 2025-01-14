db08bc9c07fc919d17abbd7c400826937ebb2d37
Christian Tabedzki <35670232+tabedzki@users.noreply.github.com>
2025-01-14 14:25:04 -0500

    Added the scheduler.TechSchedule table

---
+++ schemas/+scheduler/TechSchedule.m
@@ -0,0 +1,12 @@
+%{
+#
+date                        : date                          # Full date
+-----
+-> [nullable] lab.User      # Technician on Duty
+%}
+
+% -> tech_duties                     : varchar(255)      # tech duties
+classdef TechSchedule < dj.Manual
+end
+
+
