24151fecfdb6e0c92560a941053573ea80706a4c
Christian Tabedzki <35670232+tabedzki@users.noreply.github.com>
2025-03-10 11:54:46 -0400

    Add TechDuties schema definition for technician duties tracking

---
+++ schemas/+scheduler/TechDuties.m
@@ -0,0 +1,12 @@
+%{
+# 
+tech_duties                        : varchar(100)                          # Full date
+---
+description                 : varchar(255)
+%}
+
+
+classdef TechDuties < dj.Lookup
+
+
+end
