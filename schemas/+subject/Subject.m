c9335ef535073afe21ca9878ae3ad4db23be7053
vrrigs <70240135+vrrigs@users.noreply.github.com>
2025-04-07 19:14:19 -0400

    Added ingest new headplate images and subject new image path columns

--- schemas/+subject/Subject.m
+++ schemas/+subject/Subject.m
@@ -15,6 +15,7 @@ subject_description=''      : varchar(255)                  # description
 initial_weight=null         : float                         # initial weight of the animal before the training start.
 notification_enabled=1      : tinyint                       # Boolean to control wheter or not notifications for this subject are sent
 need_reweight=0             : tinyint                       # Boolean flag for weighting GUI to notify technician to reweight subject if too thin and extra water
+headplate_image_path=null  : varchar(4096)
 %}
 
 classdef Subject < dj.Manual
