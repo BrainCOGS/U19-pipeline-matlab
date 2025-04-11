9bbea3be5a97756b178a49094575ca85f1efa32a
vrrigs <70240135+vrrigs@users.noreply.github.com>
2025-04-11 12:17:14 -0400

    Scheduler foreign keys fix

--- schemas/+scheduler/InputOutputProfileList.m
+++ schemas/+scheduler/InputOutputProfileList.m
@@ -1,9 +1,9 @@
 %{
 # InputOutputProfile full list of InputsOutputs and type of test for each
--> lab.InputOutputProfile
+-> scheduler.InputOutputProfile
 input_output_num            : int                           # # Of Input/Output for this profile
 ---
--> lab.InputOutputRig
+-> scheduler.InputOutputRig
 check_type                  : enum('Mandatory','Optional')  # Prevent training if missing this input/output
 %}
 
--- utils/copy_sessions_test_users/create_test_subject.m
+++ utils/copy_sessions_test_users/create_test_subject.m
@@ -5,6 +5,7 @@ subject_info = fetch(subject.Subject & query_subject,'*');
 
 subject_info.subject_fullname = strrep(subject_info.subject_fullname,subject_info.user_id,'testuser');
 subject_info.subject_fullname = [subject_info.subject_fullname,'_T'];
+subject_info.subject_nickname = [subject_info.subject_nickname,'_T'];
 new_name = subject_info.subject_fullname;
 subject_info.user_id = 'testuser';
 
