acdca0b02f11222c83a718d3bf032f81607fcb10
mmagos28 <119622721+mmagos28@users.noreply.github.com>
2025-05-26 23:51:23 -0400

    session_date is char

--- scripts/populate_tables.m
+++ scripts/populate_tables.m
@@ -21,7 +21,7 @@ updateTrainingProfileProtocol;
 
 % Populate optogenetics tables
 sm = acquisition.SessionManipulation;
-ref_date = datetime(datetime('now') - days(14),'Format', 'uuuu-MM-dd');
+ref_date = char(datetime(datetime('now') - days(14),'Format', 'uuuu-MM-dd'));
 query_sessions = ['session_date > "', ref_date, '"'];
 sm.ingest_previous_optogenetic_sessions(query_sessions);
 
