d2257fb8a0b0bfb388887147d50badfd9e62efcc
mmagos28 <119622721+mmagos28@users.noreply.github.com>
2025-06-02 03:39:27 -0400

    pull Virmen every time nightcronjob is executed

--- scripts/call_u19_night_cronjob.sh
+++ scripts/call_u19_night_cronjob.sh
@@ -3,6 +3,9 @@ echo $(pwd)
 source /home/u19prod@pu.win.princeton.edu/.bashrc
 source /home/u19prod@pu.win.princeton.edu/.bash_profile
 
+cd "/home/u19prod@pu.win.princeton.edu/Datajoint_projs/ViRMEn/"
+git pull
+
 cd "/home/u19prod@pu.win.princeton.edu/Datajoint_projs/U19-pipeline-matlab/"
 git pull
 matlab -nodisplay -nosplash -nodesktop -batch "run 'scripts/startup_virtual_machine.m'; run 'scripts/populate_tables.m';"
