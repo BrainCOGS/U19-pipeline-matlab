8524c83fb56ff50d0f2bf45a2187ec78ca596212
mmagos28 <119622721+mmagos28@users.noreply.github.com>
2025-06-04 18:01:01 -0400

    TwoLicksSpoutsBlockTrial for empty blocks fix

--- schemas/+behavior_subtask/TwolickspoutsBlockTrial.m
+++ schemas/+behavior_subtask/TwolickspoutsBlockTrial.m
@@ -25,6 +25,7 @@ classdef  TwolickspoutsBlockTrial < dj.Part
             % trial_structure = structure array with trial information for the specific subtask: Twolickspouts
             
             total_trials = 0;
+            trial_structure = struct;
             nTrials = length([block_data.trial.choice]);
             for itrial = 1:nTrials
                 
