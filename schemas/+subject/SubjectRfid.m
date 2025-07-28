a9083218137fc21413f416999971936ec785f6ba
unknown <alvaroluna.pni@gmail.com>
2025-07-28 13:36:55 -0400

    Added rfid table

---
+++ schemas/+subject/SubjectRfid.m
@@ -0,0 +1,14 @@
+%{
+# 
+-> subject.Subject
+---
+rfid=null                   : varchar(8)                    # datetime when notification was inactivated
+%}
+
+
+classdef SubjectRfid < dj.Manual
+
+
+end
+
+
