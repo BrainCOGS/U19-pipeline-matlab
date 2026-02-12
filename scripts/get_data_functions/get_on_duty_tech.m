b8296857724be8b8dc760947a6dfe49945c4dff3
Alvaro Luna <alvalunasan@gmail.com>
2026-02-12 14:16:53 -0600

    Add functionality to get current technician function

--- scripts/get_data_functions/get_on_duty_tech.m
+++ scripts/get_data_functions/get_on_duty_tech.m
@@ -1,6 +1,10 @@
-function duty_tech_slack = get_on_duty_tech()
+function duty_tech = get_on_duty_tech(data_to_fetch)
 
-duty_tech_slack = {};
+if nargin < 1
+    data_to_fetch = '';
+end
+
+duty_tech = {};
 
 % Fetch technician info from WhenIWork iCal feed
 tech_info = fetchWhenIWorkTech(datetime('today'));
@@ -8,9 +12,16 @@ tech_info = fetchWhenIWorkTech(datetime('today'));
 % If a technician was found and has an ID, get their slack handle
 if ~isempty(tech_info) && isfield(tech_info, 'ID') && ~isempty(tech_info.ID)
     try
-        user_data = fetch(lab.User & sprintf('user_id = "%s"', tech_info.ID), 'slack');
-        if ~isempty(user_data)
-            duty_tech_slack = {user_data.slack};
+        if isempty(data_to_fetch)
+            user_data = fetch(lab.User & sprintf('user_id = "%s"', tech_info.ID), 'slack');
+            if ~isempty(user_data)
+                duty_tech = {user_data.slack};
+            end
+        else
+            user_data = fetch(lab.User & sprintf('user_id = "%s"', tech_info.ID), data_to_fetch{:});
+            if ~isempty(user_data)
+                duty_tech = user_data;
+            end
         end
     catch ME
         warning('get_on_duty_tech:SlackLookupError', 'Could not fetch slack info: %s', ME.message);
@@ -18,10 +29,18 @@ if ~isempty(tech_info) && isfield(tech_info, 'ID') && ~isempty(tech_info.ID)
 elseif ~isempty(tech_info) && isfield(tech_info, 'Name')
     % Try to lookup by full name if ID not available
     try
-        user_data = fetch(lab.User & sprintf('full_name = "%s"', tech_info.Name), 'slack');
-        if ~isempty(user_data)
-            duty_tech_slack = {user_data.slack};
+        if isempty(data_to_fetch)
+            user_data = fetch(lab.User & sprintf('full_name = "%s"', tech_info.Name), 'slack');
+            if ~isempty(user_data)
+                duty_tech = {user_data.slack};
+            end
+        else
+            user_data = fetch(lab.User & sprintf('full_name = "%s"', tech_info.Name), data_to_fetch{:});
+            if ~isempty(user_data)
+                duty_tech = user_data;
+            end
         end
+
     catch ME
         warning('get_on_duty_tech:SlackLookupError', 'Could not fetch slack info: %s', ME.message);
     end
@@ -35,8 +54,8 @@ end
 %         tech["start_time"] = tech["start_time"].astimezone(ZoneInfo("America/New_York"))
 %         tech["end_time"] = tech["end_time"].astimezone(ZoneInfo("America/New_York"))
 %     return tech_today
-% 
-% 
+%
+%
 % def determine_on_duty() -> str | None:
 %     tech_today = fetch_tech_today()
 %     now = datetime.now(tz=ZoneInfo("America/New_York"))
