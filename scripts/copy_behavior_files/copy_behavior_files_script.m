6d6be094a4c7066ea9ba7e55e3e64b3f1968d939
Alvaro Luna <alvalunasan@gmail.com>
2026-02-19 16:47:12 -0600

    Disable for now copyNODB Virmen files

--- scripts/copy_behavior_files/copy_behavior_files_script.m
+++ scripts/copy_behavior_files/copy_behavior_files_script.m
@@ -1,10 +1,10 @@
 
 % Script to run in cronjob to do behavior backup copying files 
 
-[status, msg] = copy_noDBVirmen_backup_files();
-if ~status
-    error_behavior_backup_notification_slack(RigParameters.rig, msg, "NODBFiles")
-end
+%[status, msg] = copy_noDBVirmen_backup_files();
+%if ~status
+%    error_behavior_backup_notification_slack(RigParameters.rig, msg, "NODBFiles")
+%end
 [final_status, msg] = copy_remote_behavior_files(RigParameters.rig);
 if ~final_status
     error_behavior_backup_notification_slack(RigParameters.rig, msg)
