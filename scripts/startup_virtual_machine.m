d7f3ab14e610f3b3d7395efe5150723e387ba819
vrrigs <70240135+vrrigs@users.noreply.github.com>
2026-03-15 12:54:58 -0400

    Fix startup virtual machine

--- scripts/startup_virtual_machine.m
+++ scripts/startup_virtual_machine.m
@@ -57,30 +57,3 @@ else
 end
 
 
-setenv('DB_PREFIX', 'u19_')
-cd(pipeline_path)
-try
-    dj.config.load()
-catch
-end
-
-%Check if external storage was set up, if not, set it.
-if ~isfield(dj.config,'stores') || ~isfield(dj.config('stores'),'extstorage')
-    dj_initial_conf()
-end
-
-cd(virmenDir);
-clearvars;
-
-connect_tech();
-
-
-for i=1:3
-    [status,out]    = system('mount_network_drives.BAT');
-    if status == 0
-        break
-    end
-    pause(1)
-end
-
-
