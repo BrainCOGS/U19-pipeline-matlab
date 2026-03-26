b70a14c228c6e13d8c85abc3d6530395686165e1
vrrigs <70240135+vrrigs@users.noreply.github.com>
2026-03-25 21:38:07 -0400

    plot histogram difficulty trials

--- scripts/plot_data/imagesc_salient_vs_distract.m
+++ scripts/plot_data/imagesc_salient_vs_distract.m
@@ -1,54 +1,68 @@
-function imagesc_salient_vs_distract(key)
+function imagesc_salient_vs_distract(session_key, block_key)
 % check_num_towers_side check if sessions have more towers on the "wrong side" of trial_type
 % Inputs
 % key = Session(s) key
 
+if nargin < 2
+    block_key = '';
+end
 
-%Fetch data
-all_sessions = fetch((behavior.TowersSession * acquisition.Session) & key, '*');
+all_sessions = fetch(acquisition.Session & session_key);
 
-% initialize data
-max_towers = 16;
-matrix_salient_vs_distract = zeros(max_towers+1,max_towers+1);
+if ~isempty(block_key)
+    all_blocks = fetch(behavior.TowersBlock & all_sessions & block_key);
+else
+    all_blocks = fetch(behavior.TowersBlock & all_sessions);
+end
+
+all_blocks_sessions = struct2table(all_blocks);
+all_blocks_sessions = ...
+    unique(all_blocks_sessions(:, {'subject_fullname', 'session_date', 'session_number'}), 'rows');
+
+%all_trials = struct2table(fetch(behavior.TowersBlockTrial & all_blocks,...
+%    'trial_type', 'cue_presence_left', 'cue_presence_right'));
+
+
+all_trials = struct2table(fetch(behavior.TowersBlockTrial & all_blocks,...
+    'trial_type', 'cue_presence_left', 'cue_presence_right','cue_pos_left','cue_pos_right'));
+
+all_trials.num_towers_l = cellfun(@sum, all_trials.cue_presence_left);
+all_trials.num_towers_r = cellfun(@sum, all_trials.cue_presence_right);
+
+all_trials.num_towers = all_trials.num_towers_l + all_trials.num_towers_r;
+
+
+all_trials.num_towers_salient = zeros(height(all_trials),1);
+all_trials.num_towers_distract = zeros(height(all_trials),1);
+
+
+all_trials(strcmp(all_trials.trial_type,'L'),'num_towers_salient') = ...
+    all_trials(strcmp(all_trials.trial_type,'L'),'num_towers_l'); 
+all_trials(strcmp(all_trials.trial_type,'L'),'num_towers_distract') = ...
+    all_trials(strcmp(all_trials.trial_type,'L'),'num_towers_r'); 
+
+all_trials(strcmp(all_trials.trial_type,'R'),'num_towers_salient') = ...
+    all_trials(strcmp(all_trials.trial_type,'R'),'num_towers_r'); 
+all_trials(strcmp(all_trials.trial_type,'R'),'num_towers_distract') = ...
+    all_trials(strcmp(all_trials.trial_type,'R'),'num_towers_l'); 
+
+max_towers = max(all_trials.num_towers_salient);
 triangular_mat = tril(ones(max_towers+1,max_towers+1));
 triangular_mat(triangular_mat==1) = -1;
 %triangular_mat(triangular_mat==0) = 0;
 
-hist_edges = [-0.5:1:max_towers+0.5];
+hist_edges = -0.5:1:max_towers+0.5;
 lims = [1 max_towers+1];
-ticks = [1:1:max_towers+1];
-lims_labels_num = [0:1:max_towers];
+ticks = 1:1:max_towers+1;
+lims_labels_num = 0:1:max_towers;
 lims_labels = string(lims_labels_num);
 
 
-for i=1:length(all_sessions)
-    
-    wrong_num_total = 0;
-    correct_num_total = 0;
-    really_wrong_num_total = 0;
-    % For trial type (1 = left, 2 = right)
-    for j=1:2
-    
-        % get left & right towers
-        idx_type_trial = find(all_sessions(i).rewarded_side == j);
-        if j==1
-            salient_towers = all_sessions(i).num_towers_l(idx_type_trial);
-            distract_towers = all_sessions(i).num_towers_r(idx_type_trial);
-        else
-            salient_towers = all_sessions(i).num_towers_r(idx_type_trial);
-            distract_towers = all_sessions(i).num_towers_l(idx_type_trial);
-        end
-
-        ho = histogram2(salient_towers,distract_towers,hist_edges,hist_edges);
-
-        matrix_salient_vs_distract = matrix_salient_vs_distract + ho.Values'; 
+ho = histogram2(all_trials.num_towers_salient,all_trials.num_towers_distract,hist_edges,hist_edges);
+matrix_salient_vs_distract = ho.Values';
 
 
-    end
-    
-end
-
-matrix_salient_vs_distract = round(matrix_salient_vs_distract./length(all_sessions));
+matrix_salient_vs_distract = ceil(matrix_salient_vs_distract./height(all_blocks_sessions));
 
 matrix_salient_vs_distract = matrix_salient_vs_distract+triangular_mat;
 
